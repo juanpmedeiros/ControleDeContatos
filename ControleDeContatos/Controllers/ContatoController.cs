5382d235eddf7115d96505e237369ed77175aed2
Juan Pedro Medeiros <jpedro.med@hotmail.com>
2023-03-28 20:23:33 -0300

    Primeiros ajustes e criação do projeto

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarContato()
+        {
+            return View();
+        }
+
+        public IActionResult EditarContato()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
